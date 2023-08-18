3257b3c089bdf53199a4c850455a77039b8f6446
caiocezzar <caiocezzarpng@gmail.com>
2023-08-17 23:30:11 -0300

    Initial commit

---
+++ DependencyInjectionExample/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using DependencyInjectionExample.Entities;
+using DependencyInjectionExample.Services;
+
+namespace DependencyInjectionExample
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter contract data");
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine() ?? string.Empty);
+            Console.WriteLine("Date (dd/MM/yyyy):");
+            DateTime contractDate = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
+            Console.Write("Contract value: ");
+            double contractValue = Double.Parse(Console.ReadLine() ?? string.Empty, CultureInfo.InvariantCulture);
+
+            Console.Write("Enter number of installments: ");
+            int months = int.Parse(Console.ReadLine() ?? String.Empty);
+
+            Contract myContract = new Contract(number, contractDate, contractValue);
+
+            ContractService contractService = new ContractService(new PayPalService());
+            contractService.ProcessContract(myContract, months);
+
+            Console.WriteLine("Installments: ");
+            foreach (Installment installment in myContract.Installments)
+            {
+                Console.WriteLine(installment);
+            }
+        }
+    }
+}
---
+++ DependencyInjectionExample/Services/IOnlinePaymentService.cs
@@ -0,0 +1,8 @@
+﻿namespace DependencyInjectionExample.Services
+{
+    public interface IOnlinePaymentService
+    {
+        double PaymentFee(double amount);
+        double Interest(double amount, int months);
+    }
+}
